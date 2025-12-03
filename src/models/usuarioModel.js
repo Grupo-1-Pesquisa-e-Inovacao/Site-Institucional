be10f0029583bc66144871682fc8f956cd8162e0
AlexTorresOP <alexsander.torres@sptech.school>
2025-12-03 17:43:30 -0300

    feat/Adicionando novas mudanças ao Front-End

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -4,10 +4,8 @@ function autenticar(email, senha) {
   console.log("ACESSEI O USUARIO MODEL\n\n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n\t\t >> verifique suas credenciais de acesso ao banco\n\t\t >> e se o servidor de seu BD está rodando corretamente.\n\nfunction entrar():", email, senha);
 
   var instrucaoSql = `
-    SELECT u.idUsuario, u.nome, u.email, u.administrador, e.nomeUf AS estado
-    FROM usuario u
-    JOIN estado e ON u.idEstado = e.idUF
-    WHERE u.email = '${email}' AND u.senha = '${senha}';
+    SELECT idUsuario, nome, email, administrador FROM usuario
+    WHERE email = '${email}' AND senha = '${senha}';
   `;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
